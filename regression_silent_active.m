4ca3bf373cab86117d4057d8faeff7c0659c3c08
pberens <pberens>
2016-08-01 11:27:01 +0200

    before resubmission

--- regression_silent_active.m
+++ regression_silent_active.m
@@ -10,14 +10,23 @@
 % 
 % For the analysis we use the primary measures, because they are easier to
 % interpret and the derived metrics are quite correlated with those.
+%
+% In the beginning, please update the file name below to the location and
+% name of the file provided as Figure 4 - Source data.
+
+source = 'burgalossi_data.xlsx';
+
+% Please note that running the shuffle analysis can take a considerable
+% amount of time (several hours).
 
 %% Regression analysis
 %
 % The regression analysis is based on sparse logistic regression with an
 % elastic-net penalty. We evaluate multiple sparsity trade-offs via nested
-% cross-validation. 
+% cross-validation.  For the main analysis, only primary morphological
+% parameters are used. Requires statistics toolbox.
 
-[data, txt] = xlsread('burgalossi_data.xlsx');
+ [data, ~] = xlsread(source);  
 
 X = zscore(data(:,12:21)); % primary morphological parameters
 y = data(:,3) > 0;         % label: silent or active
@@ -54,11 +63,12 @@ end
 
 %% Visualization
 %
-% Plot the weights with SEM over cross-validation folds
+% Plot the mean weights with SEM over cross-validation folds
+% This corresponds to Figure 4D of Diamantaki et al.
 
-f  = Figure('size',[60 50]);
+figure
 
-wm = squeeze(mean(w,2));
+wm = squeeze(median(w,2));
 ws = squeeze(std(w,[],2)/sqrt(N));
 wx = bsxfun(@plus,repmat((1:size(X,2))',1,5),-.2:0.1:.2);
 
@@ -79,21 +89,21 @@ end
 ll = legend(l);
 set(ll,'box','off','location','NorthWest')
 
-f.cleanup()
-
 %% Shuffle analysis
 %
 % To assess whether the classification performance is significantly better
 % than chance, we perform a shuffle analysis on the labels. For each
 % iteration, we shuffle the labels and reclassify using the same
-% cross-validation procedure as before.
+% cross-validation procedure as before. The analysis shows that the
+% classification performance is very unlikely to have occured by chance
+% (P=0.008). Running this analysis takes a long time.
 
-% rng(0);
+rng(0);
 
-% pc_shuffle = NaN(500,1);
+pc_shuffle = NaN(500,1);
 
-for j=159:500
-    ys = y(randperm(length(y)));        % randomly assign labels
+for j=1:500
+    ys = y(randperm(length(y)));        % randomly assign labels for each run
     yshat = NaN(N,1);
     for i=1:N
         disp([j,i])
@@ -108,5 +118,40 @@ end
 
 mean(pc_shuffle(~isnan(pc_shuffle))>=pc(2))
 
-save burgalossi pc_shuffle pc X y yhat w
+%% Control analysis
+% As a control analysis, we use all morphological parameters, not only
+% primary ones. The control shows that derived parameters such as the
+% complexity index do not add discriminatory power.
 
+[data, ~] = xlsread(source);
+
+X = zscore(data(:,12:end)); % use all morphological parameters
+y = data(:,3) > 0;         % label: silent or active
+
+N = size(X,1);
+
+% sparsity parameters
+alpha = [.05 .1 .5 .9 .95];
+A = length(alpha);
+
+% intialize
+yhat = NaN(N,A);
+w = NaN(size(X,2),N,A);
+pc_all = NaN(A,1);
+
+% fix seed
+rng(0);
+
+for a=1:A
+    for i=1:N
+        disp([a,i])
+        idx = setdiff(1:N, i);  % leave-one-out cross validation
+        [b,stats] = lassoglm(X(idx,:),y(idx),'binomial','CV',10,'alpha',alpha(a));
+        w(:,i,a) = b(:,stats.Index1SE);
+        preds = (X*w(:,i,a)) > 0;
+        yhat(i,a) = preds(i);
+    end
+    pc_all(a) = mean(yhat(:,a)==y);
+end
+
+pc_all
